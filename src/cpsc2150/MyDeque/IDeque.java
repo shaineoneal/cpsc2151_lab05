d1e78ae519a11c2b56cb18c5735013361ceaec4c
Andrianna A <anarroy@clemson.edu>
2022-09-29 10:21:48 -0400

    Added the new default methods and their contracts. Now, how to implement them? I don't know.
    Feel free to make any edits to the contracts.

--- src/cpsc2150/MyDeque/IDeque.java
+++ src/cpsc2150/MyDeque/IDeque.java
@@ -56,4 +56,66 @@ public interface IDeque {
      * @post deque = NULL
      */
     public void clear();
+
+    /** Should return a floating-point number in front of the deque
+     *  but it won't be removed from the deque.
+     *
+     * @return the floating-point number to the front of the deque.
+     * @pre deque != NULL
+     * @post floating-point number = front of the deque
+     */
+    default  Double Peek() {
+
+
+    }
+
+    /** Should return the floating-point number at the end of the deque
+     *  but it won't remove it from the deque.
+     *
+     * @return floating-point number at the end of the deque
+     * @pre deque != NULL
+     * @post Peek = the floating-point number from the end of deque.
+     */
+    default Double endOfDeque() {
+
+    }
+
+    /** Will insert 'x' at position 'pos' in the deque.
+     *
+     * @param x the double that will be inserted into 'pos'
+     * @param pos the integer that will take in 'x' as a position.
+     *
+     * @pre deque != NULL AND 'x' is a double AND 'pos' is an int
+     * @post pos = x
+     */
+    default void insert(Double x, int pos){
+
+    }
+
+    /** Will remove whatever floating-point number that was in the 'pos'
+     *  position of the deque and it should return it.
+     *
+     * @param pos holds the floating-point number that will be removed from the deque
+     * @return the value of 'pos' after removing it from the deque
+     *
+     * @pre deque != NULL AND pos is an int
+     * @post pos = floating-point number AND pos is removed from deque
+     *
+     */
+    default Double remove(int pos) {
+
+    }
+
+    /** Should return whatever floating-point number that was in the position 'pos'
+     *  but does not remove it.
+     *
+     * @param pos holds the floating-point number that was in the position of the deque
+     * @return 'pos', the floating-point number of the position of the deque.
+     *
+     * @pre  deque != NULL AND pos is an int
+     * @post pos = floating-point number
+     */
+    default Double get(int pos) {
+
+    }
 }
